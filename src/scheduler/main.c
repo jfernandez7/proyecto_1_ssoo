f3cc7e3cd1d170f117c19b20d1a3cc56a1d28650
fcartoni <fcartoni@uc.cl>
2021-09-08 10:49:58 -0300

    subir

---
+++ src/scheduler/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#include "../file_manager/manager.h"
+
+int main(int argc, char **argv)
+{
+  printf("Hello T2!\n");
+
+  InputFile *file = read_file("input.txt");
+
+  printf("Reading file of length %i:\n", file->len);
+  for (int i = 0; i < file->len; i++)
+  {
+    char **line = file->lines[i];
+    printf(
+        "\tProcess %s from factory %s has init time of %s and %s bursts.\n",
+        line[0], line[2], line[1], line[3]);
+  }
+}
